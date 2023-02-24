5da38a5b853916215fcf8b94e4040ba02a4a93bb
Diego Grille <dggrille@sanluis.edu.ar>
2023-02-24 11:12:55 -0300

    Intro con js

---
+++ js/interactividad.js
@@ -0,0 +1,15 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("gustar").innerHTML = "ESPERO QUE TE HALLA GUSTADO EL PROYECTO, LE DEDIQUE TIEMPO, PACIENCIA Y ENTUSIASMO";
+}
+
+document.getElementById("boton_alias").onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("alias").innerHTML = "ME AYUDA A TENER MAS ENERGIA Y UN MEJOR ANIMO PARA SENTIRME MEJOR";
+}
+
+document.getElementById("boton_gracias").onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("agradecimiento").innerHTML = "GRACIAS AL DOCENTE DANIEL RINALDI POR COMPARTIR TUS CONOCIMIENTOS";
+}
